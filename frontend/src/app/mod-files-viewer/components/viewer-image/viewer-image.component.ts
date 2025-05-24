38ca90fe4498f6c179f722a390abae3346b897b3
anderl <anderl.knollinger@gmail.com>
2025-05-24 16:31:05 +0200

    image viewer

--- frontend/src/app/mod-files-viewer/components/viewer-image/viewer-image.component.ts
+++ frontend/src/app/mod-files-viewer/components/viewer-image/viewer-image.component.ts
@@ -2,6 +2,7 @@ import { Component, DestroyRef, inject, Input } from '@angular/core';
 import { INode } from '../../../mod-files-data/mod-files-data.module';
 import { INodeService } from '../../../mod-files/mod-files.module';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { TitlebarService } from '../../../mod-commons/mod-commons.module';
 
 /**
  * 
@@ -15,28 +16,32 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 export class ViewerImageComponent {
 
   private destroyRef = inject(DestroyRef);
-
   private _currINode: INode = INode.empty();
 
   @Input()
   set inode(node: INode) {
     this._currINode = node;
-    this.loadSiblingImages();
+    this.titlebarSvc.subTitle = node.name;
+    if(!this.siblingsLoaded) {
+      this.loadSiblingImages();
+      this.siblingsLoaded = true;
+    }
   }
 
   get inode(): INode {
     return this._currINode;
   }
 
+  private siblingsLoaded: boolean = false;
   allImages: INode[] = new Array<INode>();
 
   /**
    * 
    * @param inodeSvc 
    */
-  constructor(private inodeSvc: INodeService) {
-
-
+  constructor(
+    private titlebarSvc: TitlebarService,
+    private inodeSvc: INodeService) {
   }
 
   /**
@@ -49,8 +54,7 @@ export class ViewerImageComponent {
       .subscribe(inodes => {
 
         this.allImages = inodes.filter(inode => { return inode.type.startsWith('image/') });
-        console.dir(this.allImages);
-      })
+      });
   }
 
   /**
