8f8e57161e81e76d5064de6d12d23c3cc4cf06e2
anderl <anderl.knollinger@gmail.com>
2025-06-23 06:03:47 +0200

    MultiNode-DnD mit eigenem DragImage

--- frontend/src/app/mod-files/directives/inode-drag-source.directive.ts
+++ frontend/src/app/mod-files/directives/inode-drag-source.directive.ts
@@ -1,9 +1,10 @@
-import { Directive, HostBinding, HostListener, Input } from '@angular/core';
+import { Directive, HostBinding, HostListener, inject, Input } from '@angular/core';
 
 import { IINode, INode } from "../../mod-files-data/mod-files-data.module";
 
 import { CheckPermissionsService } from '../services/check-permissions.service';
 import { Permissions } from '../models/permissions';
+import { ContentTypeService } from '../services/content-type.service';
 
 @Directive({
   selector: '[appINodeDragSource]',
@@ -11,8 +12,13 @@ import { Permissions } from '../models/permissions';
 })
 export class INodeDragSourceDirective {
 
-  constructor(private checkPermsSvc: CheckPermissionsService) {
+  private static multiNodeDragImage: HTMLImageElement | null = null;
 
+  constructor(
+    private checkPermsSvc: CheckPermissionsService,
+    private contentTypeSvc: ContentTypeService) {
+
+    this.ensureMultiNodeDragImageAvailable();
   }
 
   @Input()
@@ -30,15 +36,24 @@ export class INodeDragSourceDirective {
   @HostListener('dragstart', ['$event'])
   onDragStart(evt: DragEvent) {
 
-    if (evt.dataTransfer && this.checkPermsSvc.hasPermissions(Permissions.READ, this.appINodeDragSource)) {
-
-      evt.dataTransfer.clearData();
-      evt.dataTransfer.setData(INode.DATA_TRANSFER_TYPE, JSON.stringify(this.composeEventPayload()));
-      evt.dataTransfer.dropEffect = 'move';
-    }
-    else {
+    if (!evt.dataTransfer || !this.checkPermsSvc.hasPermissions(Permissions.READ, this.appINodeDragSource)) {
       evt.stopPropagation();
       evt.preventDefault();
+    }
+    else {
+
+      const allNodes = this.getAllNodes();
+      const json = allNodes.map(node => {
+        return node.toJSON();
+      })
+
+      evt.dataTransfer.clearData();
+      evt.dataTransfer.setData(INode.DATA_TRANSFER_TYPE, JSON.stringify(json));
+      evt.dataTransfer.dropEffect = 'move';
+
+      if (allNodes.length > 1 && INodeDragSourceDirective.multiNodeDragImage) {
+        evt.dataTransfer.setDragImage(INodeDragSourceDirective.multiNodeDragImage, 0, 0);
+      }
     }
   }
 
@@ -46,14 +61,38 @@ export class INodeDragSourceDirective {
    * 
    * @returns 
    */
-  private composeEventPayload(): IINode[] {
-
+  private getAllNodes(): INode[] {
     const allNodes = new Set<INode>(this.appINodeDragSourceSelectedINodes);
     allNodes.add(this.appINodeDragSource);
-    const result = Array.from(allNodes);
-    return result.map(inode => {
-      return inode.toJSON();
-    });
+    return Array.from(allNodes);
+  }
+
+  /**
+   * Liefere das Image zum anzeigen einer MultiNode-Drag-Op.
+   * 
+   * Das ganze ist nicht trivial, das Image muss zwingend ein 
+   * **sichtbares DOM-Element** des aktuellen Dokumentes sein.
+   * 
+   * Wir erzeugen also eine Singleton-Instanz eines 
+   * HTMLImgElement, fügen dieses an den document.body an
+   * und verpassen ihm einen zIndex < 0. Dadurch liegt das
+   * Teil *hinter* dem ganzen anderen Kram, der User  merkt 
+   * nichts davon und das Element gilt als "visible".
+   * 
+   * @returns 
+   */
+  private ensureMultiNodeDragImageAvailable() {
+
+    if (!INodeDragSourceDirective.multiNodeDragImage) {
+
+      const img = document.createElement('img');
+
+      img.setAttribute('style', 'position: absolute; z-index: -1');
+      img.src = this.contentTypeSvc.getTypeIconUrl('document/multiple')
+      document.body.appendChild(img);
+
+      INodeDragSourceDirective.multiNodeDragImage = img;
+    }
   }
 }
 
